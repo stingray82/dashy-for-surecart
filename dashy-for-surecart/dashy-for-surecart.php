b02d8e1dbf0b3b84f49b943eb8ba57ad26a0ef94
Stingray82 <11146912+stingray82@users.noreply.github.com>
2025-08-02 15:41:26 +0100

    Version 1.29.3 Release

--- dashy-for-surecart/dashy-for-surecart.php
+++ dashy-for-surecart/dashy-for-surecart.php
@@ -5,7 +5,7 @@
  * Tested up to:      6.8.2
  * Requires at least: 6.5
  * Requires PHP:      8.0
- * Version:           1.29.2
+ * Version:           1.29.3
  * Author:            ReallyUsefulPlugins.com
  * Author URI:        https://Reallyusefulplugins.com
  * License:           GPL-2.0-or-later
@@ -59,7 +59,7 @@ namespace rupdashextendersc\SureCartDashboard {
 
 namespace {
 
-    define('RUP_SC_D4SC_VERSION', '1.29.2');
+    define('RUP_SC_D4SC_VERSION', '1.29.3');
     
     function register_plugin_updater() {
     // 1) Load the universal drop-in.
@@ -83,10 +83,6 @@ namespace {
 add_action( 'plugins_loaded', 'register_plugin_updater', 20 );
 
 
-   
-}
-
-
 // MainWP Icon Filter
 add_filter('mainwp_child_stats_get_plugin_info', function($info, $slug) {
 
@@ -97,3 +93,9 @@ add_filter('mainwp_child_stats_get_plugin_info', function($info, $slug) {
     return $info;
 
 }, 10, 2);
+
+
+   
+}
+
+
